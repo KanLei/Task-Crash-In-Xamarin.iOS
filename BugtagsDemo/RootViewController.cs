e622d5ad89c294f20c974e5da5a883d4b660b26a
kanleilei <kanleilei@sodao.com>
2016-04-22 15:52:05 +0800

    add crash demo

---
+++ BugtagsDemo/RootViewController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UIKit;
+using BugtagsBinding.iOS;
+using System.Threading.Tasks;
+
+namespace BugtagsDemo
+{
+    public class RootViewController:UIViewController
+    {
+        public RootViewController()
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            View.BackgroundColor = UIColor.White;
+
+            TestMethodAsync();
+        }
+
+        private async Task TestMethodAsync()
+        {
+            // 1. Success
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("After Delay Thread");
+
+            // 2. Fail
+            await Task.Delay(1000);
+            Console.WriteLine("After Delay Task");
+        }
+    }
+}
+
